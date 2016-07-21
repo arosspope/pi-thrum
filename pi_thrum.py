eeebe21a7eae0d3d3b259f5f4d87a74de23aad55
Andrew <andrew.pope456@gmail.com>
2016-07-21 14:38:12 +1000

    Rename of thrum_step to just step

--- pi_thrum.py
+++ pi_thrum.py
@@ -1,15 +1,15 @@
 from modes import thrum_live
-from modes import thrum_step
+from modes import step
 
 if __name__ == "__main__":
     print("Welcome to pi-thrum! An interactive 12-step sequencer for"
           " drum samples!\nTo stop program press CTRL+C\n")
     
-    step = thrum_step.StepPlay(verbose=True)
+    program = step.StepPlay(verbose=True)
     
     try:
-        step.run()
+        program.run()
     except KeyboardInterrupt:
         print("\nThanks for Playing!")
-        step.cleanup()
+        program.cleanup()
         exit()
